a99db929abea503e070702b227a5f8a1a6e9231c
Edmond Lai <edmondklai@gmail.com>
2017-07-29 09:39:25 -0500

    Updated error checking

--- censusAPI.py
+++ censusAPI.py
@@ -1,14 +1,12 @@
 import urllib.request
-import pandas
-from config import API_KEY
+import pandas as pd
 
 
 class my_Census:
     def __init__(self, API_KEY):
         self.key = API_KEY
-        self.censusURL = 'https://api.census.gov/data/'
-        self.year = "2015"
-        self.survey = "acs5"
+        self.year = ""
+        self.survey = ""
         self.fields = []
         self.filter = []
         self.query = ""
@@ -16,6 +14,28 @@ class my_Census:
 
     def get_API_KEY(self):
         print(self.key)
+
+
+    def set_year(self, year):
+        self.year = str(year)
+
+
+    def set_survey(self, survey):
+        self.survey = str(survey)
+
+
+    def set_field(self, fields):
+        self._checkIsList(fields)
+        for field in fields:
+            self.fields.append(field)
+
+
+    def set_geoFilter(self, filter):
+        self._checkIsList(filter)
+        if type(filter) is not list:
+            raise TypeError
+        for criteria in filter:
+            self.filter.append(criteria)
 
 
     def contruct_query(self):
@@ -32,29 +52,32 @@ class my_Census:
         print(self.query)
 
 
+    def download_data(self):
+        self._checkIsEmpty()
 
-    def set_year(self, year):
-        self.year = year
-
-
-    def set_survey(self, survey):
-        self.survey = survey
-
-
-    def set_field(self, fields):
-        for field in fields:
-            self.fields.append(field)
-
-
-    def set_geoFilter(self, filter):
-        for criteria in filter:
-            self.filter.append(criteria)
-
-
-    def get(self):
         request = urllib.request.Request(self.query)
         response = urllib.request.urlopen(request)
-        print(pandas.read_json(response))
+        return(pd.read_json(response))
+
+
+    def _checkIsEmpty(self):
+        if self.year == "":
+            raise ValueError("Year has not been set")
+        elif self.survey == "":
+            raise ValueError("Survey has not been set")
+        elif self.fields == []:
+            raise ValueError("Fields have not been set")
+        elif self.set_geoFilter == []:
+            raise ValueError("Geofilter has not been set")
+
+
+    def _checkIsList(self, input):
+        if type(input) is not list:
+            raise TypeError("The input of {} must be in a list format".format(
+                str(input)
+            ))
+
+
 
 
 
